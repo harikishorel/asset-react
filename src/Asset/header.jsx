566526e1afa4e68dd0368e443ad499abf46a1949
Ragnor16 <diwaarvind6@gmail.com>
2023-02-17 16:50:04 +0530

    diwakar commit

---
+++ src/Asset/header.jsx
@@ -0,0 +1,22 @@
+import Container from "react-bootstrap/Container";
+// import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import './Header.css'
+
+
+
+function header() {
+  return (
+  	<header class="header">
+		<h1 class="logo"><a href="#">Assetwarranty</a></h1>
+      <ul class="main-nav">
+          
+          <li><a href="#">Profile</a></li>
+      </ul>
+	</header> 
+
+
+  );
+}
+
+export default header;
